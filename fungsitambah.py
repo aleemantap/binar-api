21aba673587881d64e77b5d668afb5e66f49fdfd
aleemantap <aleemantap@gmail.com>
2022-11-03 20:01:41 +0700

    push

---
+++ fungsitambah.py
@@ -0,0 +1,11 @@
+def tambah(angka_1, angka_2):
+    """Fungsi menambah 2 angka"""
+    return angka_1, angka_2
+
+
+print("masukkan angka 1: ")
+angka_1 = float(input())
+angka_2 = float(input())
+
+hasil = tambah(angka_1, angka_2)
+print(f"Hasilnya: {hasil}")
